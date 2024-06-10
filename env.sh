606ce4ac51f55fab9c5cec485c7b226ae3322adf
diegorom27 <diegorom27@gmail.com>
2024-06-10 17:26:43 -0500

    Delete env.sh

--- env.sh
+++
@@ -1,5 +0,0 @@
-# env.sh
-export APIKEY="YCCCd9JfY2JOHXndTQC6Clj5zDuqMpR9aQBUm4HUDq6E"
-export REGION="us-south"
-export id_vsi="0717_40527804-f2e7-441b-a28f-ffe34600c609"
-export VPC_API_ENDPOINT="https://us-south.iaas.cloud.ibm.com/v1"
